93effe17f4eb89a80ad1b1629c5b76504831ddcf
psegre <psegre@dc.uba.ar>
2024-06-23 16:40:10 -0300

    optimizacion de caminoOptimo esta es la buena

--- logico/tp2-pablo.pl
+++ logico/tp2-pablo.pl
@@ -105,7 +105,12 @@ tamanioTablero([F|T],A) :- length(F,B), length([F|T],H), A is B*H.
 %% Ejercicio 7
 %% caminoOptimo(+Inicio, +Fin, +Tablero, -Camino) será verdadero cuando Camino sea un
 %% camino óptimo sobre Tablero entre Inicio y Fin. Notar que puede no ser único.
-caminoOptimo(PI,PF,T,C) :- camino2(PI,PF,T,C), length(C,L), not((camino(PI,PF,T,C2), length(C2,L2), L2<L)).
+caminoOptimo(PI,PF,T,C) :- caminoOptimoAux(PI,PF,T,C,0).
+
+caminoOptimoAux(PI,PF,T,C,L) :- (length(C2,L), camino(PI,PF,T,C2) -> 
+                                       (length(C,L), camino(PI,PF,T,C))
+                                       ;
+                                       (L2 is L+1, caminoOptimoAux(PI,PF,T,C,L2))).
 
 %%%%%%%%%%%%%%%%%%%%%%%%
 %% Tableros simultáneos
